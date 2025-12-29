9f29bba8266ad6be4666430ac1d80b0cc766b1dd
myselfcarvajal <myselfcarvajal@gmail.com>
2025-12-28 19:13:07 -0500

    feat: add in-memory database

--- src/main/java/com/example/runnerz/Application.java
+++ src/main/java/com/example/runnerz/Application.java
@@ -2,6 +2,7 @@ package com.example.runnerz;
 
 import com.example.runnerz.run.Location;
 import com.example.runnerz.run.Run;
+import com.example.runnerz.run.RunRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -20,18 +21,18 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    CommandLineRunner runner() {
-        return args -> {
-            Run run = new Run(
-                    1,
-                    "Sample Run",
-                    LocalDateTime.now(),
-                    LocalDateTime.now().plusHours(1),
-                    5,
-                    Location.OUTDOOR
-            );
-            log.info("Run:{}", run);
-        };
-    }
+//    @Bean
+//    CommandLineRunner runner(RunRepository runRepository) {
+//        return args -> {
+//            Run run = new Run(
+//                    1,
+//                    "First Run",
+//                    LocalDateTime.now(),
+//                    LocalDateTime.now().plusHours(1),
+//                    5,
+//                    Location.OUTDOOR
+//            );
+//            runRepository.create(run);
+//        };
+//    }
 }
