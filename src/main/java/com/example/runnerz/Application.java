4de2581eeeed7870d8bae46b6bbf201c9f2469aa
myselfcarvajal <myselfcarvajal@gmail.com>
2025-12-31 07:53:18 -0500

    feat: add UserHttpClient interface

--- src/main/java/com/example/runnerz/Application.java
+++ src/main/java/com/example/runnerz/Application.java
@@ -1,7 +1,12 @@
 package com.example.runnerz;
 
+import com.example.runnerz.user.UserHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +14,12 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    UserHttpClient userHttpClient() {
+        RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+        return factory.createClient(UserHttpClient.class);
     }
 }
