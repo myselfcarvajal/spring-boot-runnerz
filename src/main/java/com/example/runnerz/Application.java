977865adfa9ee19531b7c9ff0ae8714fd58f1146
myselfcarvajal <myselfcarvajal@gmail.com>
2025-12-31 07:41:59 -0500

    feat: add user data models and REST client for external API integration

--- src/main/java/com/example/runnerz/Application.java
+++ src/main/java/com/example/runnerz/Application.java
@@ -1,31 +1,13 @@
 package com.example.runnerz;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-//    @Bean
-//    CommandLineRunner runner(RunRepository runRepository) {
-//        return args -> {
-//            Run run = new Run(
-//                    1,
-//                    "First Run",
-//                    LocalDateTime.now(),
-//                    LocalDateTime.now().plusHours(1),
-//                    5,
-//                    Location.OUTDOOR
-//            );
-//            runRepository.create(run);
-//        };
-//    }
 }
